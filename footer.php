7c953efeae5e95d7204d19bdace396751c13efe0
Amardeep Prakash <37177753+Amardeep1015@users.noreply.github.com>
2019-02-06 22:22:13 +0530

    Footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    <center>
+        <div class="container">
+            <p>Copyright @ Book Your Hotel.All Rights Reserved | Contact us: +91 9564552813</p>
+        </div>
+    </center>
+</div>
+
